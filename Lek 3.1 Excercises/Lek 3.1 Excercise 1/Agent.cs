a1a8d7df1e88e04fda8ca63e98821e75d679fd08
KasperMartensen <tigriss.tiger@gmail.com>
2023-02-16 10:49:07 +0100

    .

---
+++ Lek 3.1 Excercises/Lek 3.1 Excercise 1/Agent.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace Lek_3._1_Excercise_1
+{
+    public class Agent
+    {
+        string id;
+        string codeName;
+        string speciality;
+        string assignment;
+
+        public Agent()
+        {
+        }
+
+        public Agent(string aId, string aName, string aSpeciality, string aAssignment)
+        {
+            id = aId;
+            codeName = aName;
+            speciality = aSpeciality;
+            assignment = aAssignment;
+        }
+
+        public string ID
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string CodeName
+        {
+            get
+            {
+                return codeName;
+            }
+            set
+            {
+                codeName = value;
+            }
+        }
+
+        public string Speciality
+        {
+            get
+            {
+                return speciality;
+            }
+            set
+            {
+                speciality = value;
+            }
+        }
+
+        public string Assignment
+        {
+            get
+            {
+                return assignment;
+            }
+            set
+            {
+                assignment = value;
+            }
+        }
+    }
+}
